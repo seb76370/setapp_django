e573f47a68bfeefb4645dd86dd4dddca2ef36d1e
sebastien baudet <seb.baudet76@gmail.com>
2022-05-11 22:52:46 +0200

    add modification setting app

--- setapp_django.py
+++ setapp_django.py
@@ -1,70 +1,90 @@
 from logging import exception
 from pathlib import Path
+from pipes import Template
+from pprint import pprint
 import sys
 import configparser
+from settings import TEMPLATES
+from numpy import true_divide
+
+
+
+def add_Dirs_app(file :str, app : str) -> bool:
+    with open(file , "r") as f:
+        lines = f.readlines()
+
+  
+    
+
+
 dossier_courant = Path.cwd()
 file_manage = dossier_courant / "manage.py"
 
-#verification si on est bien dans le mçme dossier que manage.py
-# if not file_manage.exists():
-#     print("Mauvais dossier, le script doit ses trouvé dans le même réperoire que manage.py")
+# #verification si on est bien dans le mçme dossier que manage.py
+# # if not file_manage.exists():
+# #     print("Mauvais dossier, le script doit ses trouvé dans le même réperoire que manage.py")
+# #     exit()
+
+# # recupération du nom de l'app
+# try:
+#     app = sys.argv[1]
+# except Exception:
+#     print("Erreur vous devez indiquez le nom d'une apps")
 #     exit()
 
-# recupération du nom de l'app
-try:
-    app = sys.argv[1]
-except Exception:
-    print("Erreur vous devez indiquez le nom d'une apps")
-    exit()
 
+# # verification le dossier de l'app exist
+# dir_app = dossier_courant / app
+# if not dir_app.exists():
+#     print(f"le dossier '{dir_app}' de votre app n'existe pas")
 
-# verification le dossier de l'app exist
-dir_app = dossier_courant / app
-if not dir_app.exists():
-    print(f"le dossier '{dir_app}' de votre app n'existe pas")
+# #recuperation du nom d projets dans manage.py
+# file_manage = dossier_courant / "manage.py"
+# with open(file_manage, "r") as f:
+#     lines = f.readlines()
 
-#recuperation du nom d projets dans manage.py
-file_manage = dossier_courant / "manage.py"
-with open(file_manage, "r") as f:
-    lines = f.readlines()
+# for line in lines:
+#     if "os.environ.setdefault" in line:
+#         projet = line.replace("os.environ.setdefault('DJANGO_SETTINGS_MODULE', '","")
+#         projet = projet.replace(".settings')","")
+#         projet = projet.replace(" ","")
+#         projet = projet.replace("\n","")
+#         break
 
-for line in lines:
-    if "os.environ.setdefault" in line:
-        projet = line.replace("os.environ.setdefault('DJANGO_SETTINGS_MODULE', '","")
-        projet = projet.replace(".settings')","")
-        projet = projet.replace(" ","")
-        projet = projet.replace("\n","")
-        break
+# # creation des fichier et repertoire
+# file_a_creer = [
+#    {"path":dossier_courant / app / "urls.py" ,"data":
+# """
+# from django.urls import path
+# from django.views.defaults import server_error
+# urlpatterns = [
+#     #path('', index, name="index")
+# ]
+# """ ,"type":"file"},
+#    {"path":dossier_courant / app / "templates" / app,"data":"" ,"type":"dir"},
+#    {"path":dossier_courant / app / "static" / "css" / app ,"data":"" ,"type":"dir"},
+#    {"path":dossier_courant / app / "static" / "css" / app / "style.css","data":"" ,"type":"file"},
+#    {"path":dossier_courant / app / "static" / "script" / app,"data":"" ,"type":"dir"}
+# ]
 
-# creation des fichier et repertoire
-file_a_creer = [
-   {"path":dossier_courant / app / "urls.py" ,"data":
-"""
-from django.urls import path
-from django.views.defaults import server_error
-urlpatterns = [
-    #path('', index, name="index")
-]
-""" ,"type":"file"},
-   {"path":dossier_courant / app / "templates" / app,"data":"" ,"type":"dir"},
-   {"path":dossier_courant / app / "static" / "css" / app ,"data":"" ,"type":"dir"},
-   {"path":dossier_courant / app / "static" / "css" / app / "style.css","data":"" ,"type":"file"},
-   {"path":dossier_courant / app / "static" / "script" / app,"data":"" ,"type":"dir"}
-]
-
-for file in file_a_creer:
-    path = Path(file["path"])
-    if not path.exists():
-        print(f"création de '{path}'")
-        if file["type"] == "file":
-            path.touch()
-            if file["data"]:
-                path.write_text(file["data"])
-        if file["type"] == "dir":
-           path.mkdir(parents=True) 
-    else:
-        print(f"'{path}' existe déjà")
+# for file in file_a_creer:
+#     path = Path(file["path"])
+#     if not path.exists():
+#         print(f"création de '{path}'")
+#         if file["type"] == "file":
+#             path.touch()
+#             if file["data"]:
+#                 path.write_text(file["data"])
+#         if file["type"] == "dir":
+#            path.mkdir(parents=True) 
+#     else:
+#         print(f"'{path}' existe déjà")
     
-# #modification du fichier setting
-# file_setting = dossier_courant / projet / "settings.py"
+#modification du fichier setting
+#file_setting = dossier_courant / projet / "settings.py"
+file_setting = dossier_courant / "settings.py"
+# recherche de la lignes Templates
+
+add_Dirs_app(file_setting,"blog2")
+
 
